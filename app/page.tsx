412dfaa8d39272b48426e9144a0e725a979e6188
Shakil Nabil <shakilnabil11@gmail.com>
2026-03-05 16:39:16 -0500

    added components

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect, FormHTMLAttributes } from "react";
 import Link from "next/link";
-import { inter } from "./fonts";
 import Image from "next/image";
 
 export default function LandingPage() {
@@ -22,7 +21,7 @@ export default function LandingPage() {
         {/* Form Section */}
         <section className="relative max-w-53.75 md:max-w-sm w-full flex flex-col justify-center items-center">
           <Image
-            src="/images/LOGO.png"
+            src="/images/logo/LOGO.png"
             alt="Logo"
             width={220}
             height={10}
@@ -31,7 +30,7 @@ export default function LandingPage() {
           </Image>
 
           <Image
-            src={"/images/Coffee_splash.png"}
+            src="/images/decorations/Coffee_splash.png"
             alt="Coffee Trail"
             width={300}
             height={10}
@@ -39,7 +38,7 @@ export default function LandingPage() {
           </Image>
 
           <Image 
-            src="/images/Coffee_spill.png" 
+            src="/images/decorations/Coffee_spill.png" 
             alt="Coffee Spill"
             width={150}
             height={10}
@@ -47,7 +46,7 @@ export default function LandingPage() {
           </Image>
 
           <Image
-            src={"/images/Coffee_trail_1.png"}
+            src="/images/decorations/Coffee_trail_1.png"
             alt="Coffee Trail"
             width={62}
             height={10}
@@ -55,7 +54,7 @@ export default function LandingPage() {
           </Image>
 
           <Image
-            src={"/images/Coffee_trail_2.png"}
+            src="/images/decorations/Coffee_trail_2.png"
             alt="Coffee Trail"
             width={35}
             height={10}
@@ -63,7 +62,7 @@ export default function LandingPage() {
           </Image>
 
           <Image
-            src={"/images/Coffee_trail_3.png"}
+            src="/images/decorations/Coffee_trail_3.png"
             alt="Coffee Trail"
             width={150}
             height={10}
